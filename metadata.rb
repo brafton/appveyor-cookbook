b5149a5842ed79150cce7e3a94431fc56a7089ab
James Toyer <jamestoyer@toyertechnologies.com>
2015-02-12 16:14:00 +0000

    Version 0.4.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jamestoyer@toyertechnologies.com'
 license          'MIT'
 description      'Installs/Configures appveyor and it\'s agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.0'
+version          '0.4.1'
 
 depends          'windows', '~> 1.36'
 
